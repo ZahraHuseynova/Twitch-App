698987bd10e665effb9197c00430f2688c240287
Zahra Huseynova <zahra.huseynova@gmail.com>
2021-10-19 11:44:11 +0400

    added video player

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,11 +1,42 @@
 import { Component } from "react";
 import{fetchStream} from "../../actions"
 import{connect} from "react-redux"
+import React from "react";
+import flvJs from "flv.js"
 
 class StreamShow extends Component{
-    componentDidMount(){
-        this.props.fetchStream(this.props.match.params.id)
+    constructor(props){
+        super(props)
+        this.videoRef=React.createRef()
     }
+    componentDidMount(){
+        const{id}=this.props.match.params
+        this.props.fetchStream(id)
+        this.buildPlayer();
+
+    }
+
+    componentDidUpdate(){
+        this.buildPlayer();
+    }
+
+    componentWillUnmount(){
+        this.player.destroy();
+    }
+
+    buildPlayer(){
+        if(this.player || !this.props.stream){
+            return
+        }
+        const{id}=this.props.match.params
+        this.player=flvJs.createPlayer({
+            type:'flv',
+            url:`http://localhost:8000/live/${id}.flv`,
+        })
+        this.player.attachMediaElement(this.videoRef.current)
+        this.player.load();
+    }
+
     render(){
         if (!this.props.stream){
             return <div>Loading...</div>
@@ -14,6 +45,7 @@ class StreamShow extends Component{
         const {title,description} = this.props.stream;
         return (
             <div>
+                <video ref={this.videoRef} style={{width:'100%'}} controls/>
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
